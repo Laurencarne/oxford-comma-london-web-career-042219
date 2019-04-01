324a7d7c73d7d9ef6a66b76cccd56d8bb0d5cdd3
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-01 22:26:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-  array.join(",")
+  if array.count == 1
+    array.join(",")
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count == 3
+    array.join(", " , " and ")
+  else
+    array.join(", " , " and")
+  end
 end
