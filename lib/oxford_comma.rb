4d0b9df3ef4e5fe3c3bf16706b2d5943671e5b58
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-01 22:32:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.count == 2
     array.join(" and ")
   elsif array.count == 3
-    result = "#{array[0...-1].join(", ")} and #{array.last)}"
+    result = "#{array[0...-1].join(", ")} and #{array.last}"
   else
     array.join(", " , " and")
   end
